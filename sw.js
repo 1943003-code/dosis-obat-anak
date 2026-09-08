1ae5e1d4fbcb21a089e2f813677b5155e6697b19
1943003-code <1943003@unai.edu>
2026-09-08 09:50:48 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='dosis-anak-final-2026-09-r3';
+const C='dosis-anak-final-2026-09-r5';
 const A=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));
